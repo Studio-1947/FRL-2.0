76f4164ddb6082bc858f715ae9f3eb274d44e49a
Rahul Chettri <rahulchettri2020@gmail.com>
2026-02-10 15:43:06 +0530

    Added Dropdowns

--- src/components/LifeBalance3.tsx
+++ src/components/LifeBalance3.tsx
@@ -1,7 +1,16 @@
 import "./LifeBalance3.css";
 import DarkVeil from "./DarkVeil";
+import { useNavigate } from "react-router-dom";
 
 function LifeBalance3() {
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate(-1);
+  };
+  const handleExploreNow = () => {
+    navigate("/lifebalance3");
+  };
   return (
     <>
       <div style={{ width: "100%", minHeight: "100vh", position: "relative" }}>
@@ -22,7 +31,54 @@ function LifeBalance3() {
             alignItems: "center",
             flexDirection: "column",
           }}
-        ></div>
+        >
+          {/* Content */}
+          <div className="main-sec">
+            <div className="Lb-div1">
+              {/* Desktop CTA */}
+              <div className="back-btn">
+                <button onClick={handleBack}>
+                  <img src="assets/leftarr.png" width="20" height="20" />
+                </button>
+              </div>
+              <div className="text-div">
+                <h1>Where Are You From?</h1>
+                <div className="countryzip">
+                 
+                  <select id="countrySelect">
+                    <option value="">Select your country</option>
+                    <option value="India">India</option>
+                    <option value="United States">United States</option>
+                    <option value="United Kingdom">United Kingdom</option>
+                    <option value="Canada">Canada</option>
+                    <option value="Australia">Australia</option>
+                    <option value="Germany">Germany</option>
+                    <option value="France">France</option>
+                    <option value="Japan">Japan</option>
+                    <option value="Brazil">Brazil</option>
+                    <option value="China">China</option>
+                  </select>
+                 
+                  <select id="zipSelect" >
+                    <option value="">Select country first </option>
+                  </select>
+                </div>
+                <div className="ctacontent">
+                  <button onClick={handleExploreNow}>
+                    Next
+                    <img
+                      src="assets/rightarrow.png"
+                      alt=""
+                      width="20"
+                      height="20"
+                    />
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="rightdiv"></div>
+          </div>
+        </div>
       </div>
     </>
   );
